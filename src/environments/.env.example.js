44aaf36b6cda89f38e7d0530282f2a628659f9d6
Nimatullah Razmjo <=nimatullah.razmjo@gmail.com>
2018-02-04 19:15:40 +0430

    Add webpack configuration

---
+++ src/environments/.env.example.js
@@ -0,0 +1,7 @@
+exports.configuration = {
+    API_BASE_URL: 'http://localhost:3000/api/v1',
+    API_VERSION: 'api',
+    PER_PAGE: 10,
+    SOCKET_URL: 'http://localhost:3000'
+};
+//# sourceMappingURL=.env.example.js.map
